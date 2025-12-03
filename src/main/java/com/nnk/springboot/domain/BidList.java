4ac16cccef626d81f4cfc121363300838cedae76
Eric Bouchut <ebouchut@gmail.com>
2025-12-03 14:36:05 +0100

    feat(BidList): Add fields, DB column name mappings, accessors, constructors

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,129 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
-import java.sql.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.sql.Timestamp;
 
+/**
+ * A domain object (entity) modeling a list of bids.
+ * <p>
+ * I explicitly declare the mapping field name to database column name
+ * with <code>@Table(name= "...")</code>,
+ * just for the sake of practicing even if this is redundant,
+ * because the database column names  follow the Spring Boot convention.
+ * <p>
+ *  Why NOT annotating this entity class with <b><copde>@Data</copde></b> because:
+ *   - <code>toString()</code> can trigger lazy-loading issues
+ *   - <code>equals()/hashCode()</code> with all fields is problematic as this entity can mutate.
+ *   - This can cause infinite recursion with bidirectional relationships!!!
+ */
 @Entity
 @Table(name = "bid_list")
+@Getter // Call the Lombok annotation family to the rescue ;-)
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+    @Id  // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment
+    @Column(name="bid_list_id") // Map the field name to a DB column name
+    @EqualsAndHashCode.Include
+    private Integer bidListId;
+
+    @Column(name = "account", nullable = false)
+    @NotBlank
+    private String account;
+
+    @Column(name = "type", nullable = false)
+    @NotBlank
+    private String type;
+
+    @Column(name="bid_quantity")
+    @PositiveOrZero
+    private Double bidQuantity;
+
+    @Column(name="ask_quantity")
+    @PositiveOrZero
+    private Double askQuantity;
+
+    @Column(name = "bid", nullable = false)
+    @NotNull
+    @Positive
+    private Double bid;
+
+    @Column(name = "ask")
+    private Double ask;
+
+    @Column(name = "benchmark")
+    private String benchmark;
+
+    @Column(name = "bid_list_date")
+    private Timestamp bidListDate;
+
+    @Column(name = "commentary")
+    private String commentary;
+
+    @Column(name = "security")
+    private String security;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "trader")
+    private String trader;
+
+    @Column(name = "book")
+    private String book;
+
+    /**
+     * Who created this <code>BidList</code>.
+     */
+    @Column(name = "creation_name")
+    @CreatedBy
+    private String creationName;
+
+    /**
+     * When was this <code>BidList</code> created
+     */
+    @Column(name = "creation_date", nullable = false)
+    @CreatedDate
+    private Timestamp creationDate;
+
+    /**
+     * Who updated this <code>BidList</code>.
+     */
+    @Column(name="revision_name")
+    private String revisionName;
+
+    /**
+     * When was this <code>BidList</code> updated
+     */
+    @Column(name = "revision_date", nullable = false)
+    private Timestamp revisionDate;
+
+    @Column(name = "deal_name")
+    private String dealName;
+
+    @Column(name = "deal_type")
+    private String dealType;
+
+    /**
+     * Exclude this field from toString(),
+     * assuming this field is a foreign key to another entity.
+     */
+    @Column(name = "source_list_id")
+    @ToString.Exclude
+    private String sourceListId;
+
+    @Column(name = "side")
+    private String side;
 }
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,103 +1,94 @@
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- src/main/resources/db/schema.sql
- --
+--
 -- (DDL Creation script)
 -- This script creates the tables in the poseidon database
--- using an exotic naming convention!
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 CREATE TABLE bid_list (
-  bid_list_id tinyint(4) NOT NULL AUTO_INCREMENT,
-  account VARCHAR(30) NOT NULL,
-  type VARCHAR(30) NOT NULL,
-  bid_quantity DOUBLE,
-  ask_quantity DOUBLE,
-  bid DOUBLE ,
-  ask DOUBLE,
-  benchmark VARCHAR(125),
-  bid_list_date TIMESTAMP,
-  commentary VARCHAR(125),
-  security VARCHAR(125),
-  status VARCHAR(10),
-  trader VARCHAR(125),
-  book VARCHAR(125),
-  creation_name VARCHAR(125),
-  creation_date TIMESTAMP ,
-  revision_name VARCHAR(125),
-  revision_date TIMESTAMP ,
-  deal_name VARCHAR(125),
-  deal_type VARCHAR(125),
-  source_list_id VARCHAR(125),
-  side VARCHAR(125),
-
-  PRIMARY KEY (bid_list_id)
+    bid_list_id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
+    account VARCHAR(30) NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    bid_quantity DOUBLE,
+    ask_quantity DOUBLE,
+    bid DOUBLE,
+    ask DOUBLE,
+    benchmark VARCHAR(125),
+    bid_list_date TIMESTAMP,
+    commentary VARCHAR(125),
+    security VARCHAR(125),
+    status VARCHAR(10),
+    trader VARCHAR(125),
+    book VARCHAR(125),
+    creation_name VARCHAR(125),
+    creation_date TIMESTAMP ,
+    revision_name VARCHAR(125),
+    revision_date TIMESTAMP ,
+    deal_name VARCHAR(125),
+    deal_type VARCHAR(125),
+    source_list_id VARCHAR(125),
+    side VARCHAR(125)
 );
 
 CREATE TABLE trade (
-  TradeId tinyint(4) NOT NULL AUTO_INCREMENT,
-  account VARCHAR(30) NOT NULL,
-  type VARCHAR(30) NOT NULL,
-  buyQuantity DOUBLE,
-  sellQuantity DOUBLE,
-  buyPrice DOUBLE ,
-  sellPrice DOUBLE,
-  tradeDate TIMESTAMP,
-  security VARCHAR(125),
-  status VARCHAR(10),
-  trader VARCHAR(125),
-  benchmark VARCHAR(125),
-  book VARCHAR(125),
-  creationName VARCHAR(125),
-  creationDate TIMESTAMP ,
-  revisionName VARCHAR(125),
-  revisionDate TIMESTAMP ,
-  dealName VARCHAR(125),
-  dealType VARCHAR(125),
-  sourceListId VARCHAR(125),
-  side VARCHAR(125),
-
-  PRIMARY KEY (TradeId)
+    trade_id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
+    account VARCHAR(30) NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    buy_quantity DOUBLE,
+    sell_quantity DOUBLE,
+    buy_price DOUBLE ,
+    sell_price DOUBLE,
+    trade_date TIMESTAMP,
+    security VARCHAR(125),
+    status VARCHAR(10),
+    trader VARCHAR(125),
+    benchmark VARCHAR(125),
+    book VARCHAR(125),
+    creation_name VARCHAR(125),
+    creation_date TIMESTAMP ,
+    revision_name VARCHAR(125),
+    revision_date TIMESTAMP ,
+    deal_name VARCHAR(125),
+    deal_type VARCHAR(125),
+    source_list_id VARCHAR(125),
+    side VARCHAR(125)
 );
 
 CREATE TABLE curve_point (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  CurveId tinyint,
-  asOfDate TIMESTAMP,
-  term DOUBLE ,
-  value DOUBLE ,
-  creationDate TIMESTAMP ,
+    id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
 
-  PRIMARY KEY (Id)
+    curve_id tinyint,
+    as_of_date TIMESTAMP,
+    term DOUBLE ,
+    value DOUBLE ,
+    creation_date TIMESTAMP
 );
 
 CREATE TABLE rating (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  moodysRating VARCHAR(125),
-  sandPRating VARCHAR(125),
-  fitchRating VARCHAR(125),
-  orderNumber tinyint,
+    id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
 
-  PRIMARY KEY (Id)
+    moodys_rating VARCHAR(125),
+    sand_prating VARCHAR(125),
+    fitch_rating VARCHAR(125),
+    order_number tinyint
 );
 
 CREATE TABLE rule_name (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(125),
-  description VARCHAR(125),
-  json VARCHAR(125),
-  template VARCHAR(512),
-  sqlStr VARCHAR(125),
-  sqlPart VARCHAR(125),
+    id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
 
-  PRIMARY KEY (Id)
+    name VARCHAR(125),
+    description VARCHAR(125),
+    json VARCHAR(125),
+    template VARCHAR(512),
+    sql_str VARCHAR(125),
+    sql_part VARCHAR(125)
 );
 
 CREATE TABLE user (
-  Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(125),
-  password VARCHAR(125),
-  fullname VARCHAR(125),
-  role VARCHAR(125),
+    id tinyint(4) PRIMARY KEY AUTO_INCREMENT,
 
-  PRIMARY KEY (Id)
+    user_name VARCHAR(125),
+    password VARCHAR(125),
+    full_name VARCHAR(125),
+    role VARCHAR(125)
 );
