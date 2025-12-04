689f7a400418d0778d4ea13c64119b2e897d1928
Eric Bouchut <ebouchut@gmail.com>
2025-12-04 11:06:56 +0100

    feat(User): Add the User.id field to column name mapping

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) // Only consider the id attribute/field
 @ToString
 public class User {
+    @Column(name = "id")
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
